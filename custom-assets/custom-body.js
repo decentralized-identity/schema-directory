d1eb608a56874017e784fa3fb2ceaddea1ebc471
Daniel Buchner <danieljb2@gmail.com>
2022-10-18 20:16:28 -0500

    make sorting great again

--- custom-assets/custom-body.js
+++ custom-assets/custom-body.js
@@ -5,8 +5,8 @@ DOM.delegateEvent('pointerdown', 'table:has(~ h2:not(#schemas)) td a', (e, node)
   node.setAttribute('target', '_blank');
 })
 
-
 document.querySelectorAll('#schema_tables table').forEach(table => {
+  console.log(table);
+  table.firstElementChild.firstElementChild.lastElementChild.setAttribute('data-sort-method', 'none');
   new Tablesort(table);
 })
-
